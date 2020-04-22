914a00b3d614b6a40e4ff05da38396130358aa25
Dralus15 <v.lefillastre@gmail.com>
2020-04-22 16:16:15 +0200

    private filter

---
+++ src/main/java/fr/polytech/rlcalm/controllers/PrivateFilter.java
@@ -0,0 +1,28 @@
+package fr.polytech.rlcalm.controllers;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+@WebFilter(
+        filterName = "PrivateFilter",
+        servletNames = {"MatchEditController", "PlayerEditController", "MatchScoreEditController"}
+)
+public class PrivateFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        Object user = req.getSession().getAttribute("connected");
+        if (Objects.isNull(user)) {
+            res.sendRedirect("/RamenerLaCoupeALaMaison_war_exploded/index.jsp");
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
+
